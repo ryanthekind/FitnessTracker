dad0c4975dd0847c4c4973140a10dfa02892a060
Ryan Burns <mr.ryan.burns@outlook.com>
2024-05-13 22:06:30 -0400

    third commit

--- src/com/ryanthekind/Main.java
+++ src/com/ryanthekind/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to my fitness app!");
+		
+		System.out.println("Workout 1:");
+		System.out.println("------");
+		System.out.println("REPS:");
+		
+		
+		// make a four star right triangle
+		for(int i=1; i<=4; i++) {
+			System.out.print(i);
+			
+			for (int k=1; k<=i; k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
 
 	}
 
